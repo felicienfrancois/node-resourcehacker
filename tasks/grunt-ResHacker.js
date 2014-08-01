902084abfab22c0993ed9d6ae727f47c53e40b60
Félicien FRANCOIS <felicien@tweakstyle.com>
2014-08-01 18:45:13 +0200

    typo fix

--- tasks/grunt-ResHacker.js
+++ tasks/grunt-ResHacker.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = function(grunt) {
 
-	grunt.registerMultiTask('reshacker', 'Node wrapper to edit windows executable resources', function() {
+	grunt.registerMultiTask('resourcehacker', 'Node wrapper to edit windows executable resources', function() {
 		var resHacker = require("../lib/ResHacker.js");
 		var done = this.async();
 		resHacker(this.data.command, function(error) {
