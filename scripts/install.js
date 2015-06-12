a6f8224d147b0a91912ae7adacb35fadafa7051f
Félicien FRANCOIS <felicien@tweakstyle.com>
2015-06-12 13:15:55 +0200

    - cleaned & refactored install script - handled errors - don't download if bin already in cache

--- scripts/install.js
+++ scripts/install.js
@@ -1,33 +1,33 @@
-var http = require('http')
+var http = require('http');
 var fs = require('fs');
-var AdmZip = require('adm-zip')
+var AdmZip = require('adm-zip');
 var path = require('path');
 
-var download = function(url, dest, cb) {
-  console.log('Downloading ResourceHacker by Angus Johnson...')
-  var file = fs.createWriteStream(dest);
-  var request = http.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close(cb);
-    });
-  }).on('error', function(err) { // Handle errors
-    fs.unlink(dest); 
-    if (cb) cb(err.message);
-  });
-};
+var download_url = 'http://www.angusj.com/resourcehacker/resource_hacker.zip';
+var dir_path = path.join(__dirname,'../bin/');
+var zip_path = path.join(__dirname,'../bin/resource_hacker.zip');
+var bin_path = path.join(__dirname,'../bin/ResourceHacker.exe');
 
-
-var dir_path = path.join(__dirname,'../bin/')
-var zip_path = path.join(__dirname,'../bin/resource_hacker.zip')
 if(!fs.existsSync(dir_path)){
-  fs.mkdirSync(dir_path)
+  fs.mkdirSync(dir_path);
 }
-var download_url = 'http://www.angusj.com/resourcehacker/resource_hacker.zip'
-download(download_url,zip_path,function(err){
-  var zip  = new AdmZip(zip_path)
-  zip.extractAllTo(dir_path,true)
-  console.log('Download completed')
-})
 
-
+if(!fs.existsSync(bin_path)) {
+	console.log('Downloading ResourceHacker by Angus Johnson...')
+	var file = fs.createWriteStream(zip_path);
+	var request = http.get(download_url, function(response) {
+	  response.pipe(file);
+	  file.on('finish', function() {
+	    file.close(function(err){
+	      var zip  = new AdmZip(zip_path);
+	      zip.extractAllTo(dir_path, true);
+	      console.log('Download complete');
+	      process.exit(0);
+	    });
+	  });
+	}).on('error', function(err) { // Handle errors
+	  fs.unlink(zip_path);
+	  console.error(err.message);
+	  process.exit(-1);
+	});
+}
