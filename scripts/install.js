4ec4f97655592bb9fa860eb95723f35d5c3e3375
Félicien François <felicien@tweakstyle.com>
2016-02-10 16:38:22 +0100

    proxy support

--- scripts/install.js
+++ scripts/install.js
@@ -1,6 +1,4 @@
-var http = require('http');
 var fs = require('fs');
-var AdmZip = require('adm-zip');
 var path = require('path');
 
 var download_url = 'http://www.angusj.com/resourcehacker/resource_hacker.zip';
@@ -9,25 +7,31 @@ var zip_path = path.join(__dirname,'../bin/resource_hacker.zip');
 var bin_path = path.join(__dirname,'../bin/ResourceHacker.exe');
 
 if(!fs.existsSync(dir_path)){
-  fs.mkdirSync(dir_path);
+	fs.mkdirSync(dir_path);
 }
 
-if(!fs.existsSync(bin_path)) {
-	console.log('Downloading ResourceHacker by Angus Johnson...')
-	var file = fs.createWriteStream(zip_path);
-	var request = http.get(download_url, function(response) {
-	  response.pipe(file);
-	  file.on('finish', function() {
-	    file.close(function(err){
-	      var zip  = new AdmZip(zip_path);
-	      zip.extractAllTo(dir_path, true);
-	      console.log('Download complete');
-	      process.exit(0);
-	    });
-	  });
-	}).on('error', function(err) { // Handle errors
-	  fs.unlink(zip_path);
-	  console.error(err.message);
-	  process.exit(-1);
-	});
+if(fs.existsSync(bin_path)) {
+	return;
 }
+
+var http = require('http');
+http.globalAgent = require("caw")();
+var AdmZip = require('adm-zip');
+
+console.log('Downloading ResourceHacker by Angus Johnson...')
+var file = fs.createWriteStream(zip_path);
+var request = http.get(download_url, function(response) {
+	response.pipe(file);
+	file.on('finish', function() {
+		file.close(function(err){
+			var zip	= new AdmZip(zip_path);
+			zip.extractAllTo(dir_path, true);
+			console.log('Download complete');
+			process.exit(0);
+		});
+	});
+}).on('error', function(err) { // Handle errors
+	fs.unlink(zip_path);
+	console.error(err.message);
+	process.exit(-1);
+});
